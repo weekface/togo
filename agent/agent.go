0d9f5533e5dc21e6745cede8419cffa686950d9c
weekface <weekface@gmail.com>
2014-09-04 10:58:19 +0800

    So, we will use termbox

--- agent/agent.go
+++ agent/agent.go
@@ -1,24 +1,83 @@
 package agent
 
 import (
-	"fmt"
 	"os"
 
+	"github.com/nsf/termbox-go"
 	"github.com/weekface/togo/ui"
 )
 
-type Agent struct {
-	Ui   ui.DefaultUi
-	Args map[string]interface{}
+var help = `
+Usage:
+	
+  togo --server
+
+Options:
+	
+  -s --server  Start server also.
+	
+Press Ctr+Q to quit.
+`
+
+func setString(str string, fg, bg termbox.Attribute) {
+	cols, lines := termbox.Size()
+
+	x := 0
+	y := 0
+
+	for _, r := range str {
+		if x >= cols {
+			x = 0
+			y++
+		}
+		if y >= lines {
+			break
+		}
+		if r != '\n' {
+			termbox.SetCell(x, y, r, fg, bg)
+			x++
+		} else {
+			y++
+			x = 0
+		}
+	}
 }
 
-func New(args map[string]interface{}) *Agent {
+func drawHelp() {
+	bg := termbox.ColorBlack
+	fg := termbox.ColorWhite
+
+	termbox.Clear(fg, bg)
+	setString(help, fg, bg)
+	termbox.Flush()
+}
+
+type Agent struct {
+	Ui ui.DefaultUi
+}
+
+func New() *Agent {
 	ui := ui.DefaultUi{Reader: os.Stdin, Writer: os.Stdout}
-	return &Agent{Ui: ui, Args: args}
+	return &Agent{Ui: ui}
 }
 
 func (agent *Agent) Run() {
-	if agent.Args["list"].(bool) {
-		fmt.Println("listing...")
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	defer termbox.Close()
+	drawHelp()
+
+loop:
+
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			if ev.Key == termbox.KeyCtrlQ {
+				break loop
+			}
+		}
 	}
 }
--- cmd/togo/togo.go
+++ cmd/togo/togo.go
@@ -1,33 +1,10 @@
 package main
 
 import (
-	"github.com/docopt/docopt-go"
 	"github.com/weekface/togo/agent"
 )
 
-const Usage = `togo cli.
-
-Usage:
-  togo list
-  togo add    <message>
-  togo rm     <id>
-  togo finish <id>
-  togo show   <id>
-  togo server <port> [-d|--deamon]
-  togo -h | --help
-  togo --version
-
-Options:
-  -d --deamon   Start the web server as a deamon.
-  -h --help     Show this screen.
-  --version     Show version.`
-
 func main() {
-	arguments, err := docopt.Parse(Usage, nil, true, "Togo 0.0.1", false)
-	if err != nil {
-		panic(err)
-	}
-
-	ag := agent.New(arguments)
+	ag := agent.New()
 	ag.Run()
 }
