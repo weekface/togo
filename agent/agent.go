020b01b6abd4b299509955f34284721f212066da
weekface <weekface@gmail.com>
2014-09-05 19:10:36 +0800

    add termbox inut box to accept user's inputs

--- agent/agent.go
+++ agent/agent.go
@@ -2,7 +2,9 @@ package agent
 
 import (
 	"os"
+	"unicode/utf8"
 
+	"github.com/mattn/go-runewidth"
 	"github.com/nsf/termbox-go"
 	"github.com/weekface/togo/ui"
 )
@@ -19,46 +21,47 @@ Options:
 Press Ctr+Q to quit.
 `
 
-func setString(str string, fg, bg termbox.Attribute) {
-	cols, lines := termbox.Size()
-
-	x := 0
-	y := 0
-
-	for _, r := range str {
-		if x >= cols {
-			x = 0
-			y++
+func setString(str string, fg, bg termbox.Attribute, x int, y int) int {
+	rw := 0
+	for len(str) > 0 {
+		c, w := utf8.DecodeRuneInString(str)
+		if c == utf8.RuneError {
+			c = '?'
+			w = 1
 		}
-		if y >= lines {
-			break
-		}
-		if r != '\n' {
-			termbox.SetCell(x, y, r, fg, bg)
-			x++
-		} else {
-			y++
-			x = 0
-		}
+		str = str[w:]
+		termbox.SetCell(x, y, c, fg, bg)
+		x += runewidth.RuneWidth(c)
+		rw += runewidth.RuneWidth(c)
 	}
-}
-
-func drawHelp() {
-	bg := termbox.ColorBlack
-	fg := termbox.ColorWhite
-
-	termbox.Clear(fg, bg)
-	setString(help, fg, bg)
-	termbox.Flush()
+	return rw
 }
 
 type Agent struct {
-	Ui ui.DefaultUi
+	Ui    ui.DefaultUi
+	Chars string
 }
 
 func New() *Agent {
 	ui := ui.DefaultUi{Reader: os.Stdin, Writer: os.Stdout}
 	return &Agent{Ui: ui}
+}
+
+func (agent *Agent) drawPromp(str string) {
+	agent.Chars = agent.Chars + string(str)
+
+	prompStr := "TOGO> " + agent.Chars
+
+	bg := termbox.ColorBlack
+	fg := termbox.ColorWhite
+	x := 0
+	y := 0
+
+	termbox.Clear(fg, bg)
+	width := setString(prompStr, fg, bg, x, y)
+	termbox.SetCursor(width, 0)
+	setString("Press Ctr+Q to quit.", termbox.ColorBlack, termbox.ColorWhite, 0, 2)
+	termbox.Flush()
 }
 
 func (agent *Agent) Run() {
@@ -68,15 +71,22 @@ func (agent *Agent) Run() {
 	}
 
 	defer termbox.Close()
-	drawHelp()
+	agent.drawPromp("")
+
+	setString("Press Ctr+Q to quit.", termbox.ColorBlack, termbox.ColorWhite, 0, 2)
+	termbox.Flush()
 
 loop:
 
 	for {
 		switch ev := termbox.PollEvent(); ev.Type {
 		case termbox.EventKey:
-			if ev.Key == termbox.KeyCtrlQ {
+			switch ev.Key {
+			case termbox.KeyCtrlQ:
 				break loop
+			default:
+				str := string(ev.Ch)
+				agent.drawPromp(str)
 			}
 		}
 	}
